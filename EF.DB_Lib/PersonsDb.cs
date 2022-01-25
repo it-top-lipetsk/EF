5e9867ba17e2eaed1f4a3a237494d3e56e5ec6a0
Starinin Andrey <starininandrey@gmail.com>
2022-01-25 20:01:23 +0300

    Add EF.DB_Lib

---
+++ EF.DB_Lib/PersonsDb.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using EF.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF.DB_Lib
+{
+    public class PersonsDb : DbContext
+    {
+        public DbSet<Person> TablePersons { get; set; }
+
+        public PersonsDb()
+        {
+            Database.EnsureCreated();
+        }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL(GetConnectionString("connection_to_db.txt"));
+        }
+
+        private static string GetConnectionString(string path)
+        {
+            return File.ReadAllText(path);
+        }
+    }
+}
